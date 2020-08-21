0aa8b9d731656f41d2a3d4b0a4a17e40f9538780
Chris Yoon <chy.yoon@gmail.com>
2020-08-21 08:45:48 -0700

    add animal model file and properties

---
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelterApi.Models
+{
+    public class  AnimalShelterApiContext : DbContext
+    {
+        public AnimalShelterApiContext(DbContextOptions< AnimalShelterApiContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
